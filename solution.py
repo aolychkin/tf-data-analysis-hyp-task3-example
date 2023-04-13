4f6cb5b90dae2592bc9d7ee45a3cd22325d74980
Алексей <55784938+aolychkin@users.noreply.github.com>
2023-04-13 22:53:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,10 +5,7 @@ from scipy.stats import ttest_ind
 
 chat_id = 1068310526 # Ваш chat ID, не меняйте название переменной
 
-def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+def solution(x: np.array, y: np.array) -> bool:
+    result = ttest_ind(x, y)
     alpha = 0.01
-    p_value = ttest_ind(x, y, alternative="greater").pvalue
-    return p_value < alpha
+    return result[1] < alpha
