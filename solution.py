56efa4bd535d299a0ae1ae7055bb7ae0b9a3189b
Алексей <55784938+aolychkin@users.noreply.github.com>
2023-04-13 17:00:23 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
-import statsmodels.stats.weightstats as w
+from scipy.stats import ttest_ind
 
 
 chat_id = 1068310526 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    _, pvalue = w.ztest(x, value=500, alternative='larger')
-    return pvalue <= 0.01
+    alpha = 0.01
+    p_value = ttest_ind(x, y, alternative="greater").pvalue
+    return p_value < alpha
